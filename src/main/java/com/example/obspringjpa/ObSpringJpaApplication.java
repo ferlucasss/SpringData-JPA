3617171f79a53b3450b08f3a4ab71172ae1a499e
Lucas Fernandez <lucasfernandezepe@gmail.com>
2022-08-13 18:23:33 -0300

    Initial commit

---
+++ src/main/java/com/example/obspringjpa/ObSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.obspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObSpringJpaApplication.class, args);
+	}
+
+}
